726f6f471bee552ec4108916a4f7610cc9d5c1ce
Alex Bakhteyev <a.bakhteyev@gmail.com>
2020-02-10 14:01:57 -0500

    removed colsole logs

--- src/App.js
+++ src/App.js
@@ -6,33 +6,24 @@ import { uuid } from "uuidv4";
 
 export default class App extends Component {
   state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" }
-    ],
+    contacts: [],
     filter: ""
   };
 
-  componentDidMount(){
-    const prevContacts = localStorage.getItem('contacts');
+  componentDidMount() {
+    const prevContacts = localStorage.getItem("contacts");
     if (prevContacts) {
-      console.log(prevContacts);
       this.setState({
-        contacts:JSON.parse(prevContacts),
-      })
+        contacts: JSON.parse(prevContacts)
+      });
     }
-   
-
   }
-  componentDidUpdate(prevProps,prevState){
+  componentDidUpdate(prevProps, prevState) {
     console.log("componentDidUpdate");
-  
-  if (prevState.contacts !== this.state.contacts){
-    
-    localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
-  }
+
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
   }
 
   onAddContacts = (name, number) => {
